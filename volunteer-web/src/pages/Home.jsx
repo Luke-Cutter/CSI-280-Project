c8c8c43694d7cf3e6c7cb51c08f9ae560fbfcfe1
Alex Cattani <a.cattani44@gmail.com>
2024-02-19 21:15:23 -0500

    Added Basic Login Page

--- volunteer-web/src/pages/Home.jsx
+++ volunteer-web/src/pages/Home.jsx
@@ -29,6 +29,8 @@ export default function Home() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
