68280ca87dd6fe8ae3dbeb17b71cf23c55b6ebfe
mkerwood <michael.kerwood@mymail.champlain.edu>
2024-03-25 18:02:46 -0400

    updated calendar

--- volunteer-web/src/components/Calendar.jsx
+++ volunteer-web/src/components/Calendar.jsx
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
-import CalendarDays from './CalendarDays';
-import './Calendar.css';
 
-// followed tutorial from https://derrickotte.medium.com/how-to-create-a-calendar-from-scratch-in-react-1f2db197454d
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-export default class Calendar extends Component {
-    constructor() {
-        super();
+const localizer = momentLocalizer(moment);
 
-        this.weekdays=['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        this.months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        this.state={currentDay: new Date()};
-    }
+function BigCalendar(){
 
-    changeCurrentDay = (day) => {
-        this.setState({ currentDay: new Date(day.year, day.month, day.number) });
-    }
+    const myEventsList = [];
 
-    render() {
-        return (
-            <div className="calendar">
-                <div className="calendar-header">
-                    <h2>{this.months[this.state.currentDay.getMonth()]} {this.state.currentDay.getFullYear()}</h2>
-                </div>
-                <div className="calendar-body">
-                    <div className="table-header">
-                        {
-                            this.weekdays.map((weekday, index) => {
-                                return <div key={index} className="weekday">{weekday}</div>
-                            })
-                        }
-                    </div>
-                    <CalendarDays day={this.state.currentDay} changeCurrentDay={this.changeCurrentDay} />
-                </div>
-                <div>
-                </div>
-            </div>
-        ); 
-    }
+    const MyCalendar = (props) => (
+        <div>
+            <Calendar 
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            style={{height: 500}}
+            />
+        </div>
+    )
+    
+    return(
+        <div>
+            <MyCalendar />
+        </div>
+    )
 }
+
+export default BigCalendar;
