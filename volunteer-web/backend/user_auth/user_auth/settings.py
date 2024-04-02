d7a34882eb67a02bb4c0112bb477b5bd7ab69956
Alex Cattani <a.cattani44@gmail.com>
2024-04-02 19:28:17 -0400

    Updated database settings and frontend so it works with Django

--- volunteer-web/backend/user_auth/user_auth/settings.py
+++ volunteer-web/backend/user_auth/user_auth/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'database280.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'charitiesDB',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- volunteer-web/src/pages/Signup.jsx
+++ volunteer-web/src/pages/Signup.jsx
@@ -1,5 +1,25 @@
-export default function Signup() {
-    return (
-    <h1>Signup</h1>
-    )
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DjangoTest() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/user/')
+      .then(response => {
+        setMessage(response.data.user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Dev Test</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
+
+export default DjangoTest;
