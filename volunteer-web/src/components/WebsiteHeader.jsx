91c4bba0e1606edf69c1c0765fbc32257781fef5
Alex Cattani <a.cattani44@gmail.com>
2024-04-15 20:22:07 -0400

    Added geolocation data to header and formatted

--- volunteer-web/src/components/WebsiteHeader.jsx
+++ volunteer-web/src/components/WebsiteHeader.jsx
@@ -1,13 +1,60 @@
-import vhLogo from '/vhlogofinal.svg'
+import vhLogo from '/vhlogofinal.svg';
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import "./WebsiteHeader.css";
+
+const LocationAPIKey = "enter_api_key";
 
 export default function WebsiteHeader() {
+
+    const [userLocation, setUserLocation] = useState(null);
+
+    useEffect(() => {
+      getCoordinates();
+    }, []);
+
+    function getLocationInfo(latitude, longitude) {
+      const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&key=${LocationAPIKey}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          if (data.status.code === 200) {
+            console.log("results:", data.results);
+            setUserLocation(data.results[0].components.city + ", " + data.results[0].components.state_code);
+          } else {
+            setUserLocation("Unknown Location");
+            console.log("Reverse geolocation request failed.");
+          }
+        })
+        .catch((error) => console.error(error));
+      };
+
+    const getCoordinates = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            getLocationInfo(latitude, longitude);
+          },
+          (error) => {
+            setUserLocation("Unknown Location");
+            console.error("Error getting user location: ", error);
+          }
+        );
+      }
+      else {
+        setUserLocation("Unknown Location");
+        console.log("Geolocation is not supported by this browser.");
+      }
+    };
+
     return (
-        <div>
-        <Link to="/">
-          <img src={vhLogo} className="logo" alt="Vite logo" />
-        </Link>
-        <span>Website Header</span>
+        <div className="header">
+          <Link to="/">
+            <img src={vhLogo} className="logo" alt="VH Logo" />
+          </Link>
+          <span className="locationTag">{userLocation}</span>
         </div>
     )
-}
+};
