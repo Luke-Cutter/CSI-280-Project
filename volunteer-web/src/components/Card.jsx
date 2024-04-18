c7a348dce21beaf937fe55c7682fc73e5c0d2469
Alex Cattani <a.cattani44@gmail.com>
2024-04-18 13:53:50 -0400

    Same as above

--- volunteer-web/src/components/Card.jsx
+++ volunteer-web/src/components/Card.jsx
@@ -1,7 +1,7 @@
 {/* This card will be used for charities, events, and jobs.
     If necessary, an enum can specify the design for each card.
     For now they will all have the same design. 
-    Props: image, title, overview, buttonLink, buttonText   */}
+    Props: image, title, overview, color, buttonLink, buttonText   */}
 
     export default function Card(props) {
 
