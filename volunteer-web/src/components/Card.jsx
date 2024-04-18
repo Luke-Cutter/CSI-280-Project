34093690860fbb14ad59662b4b373c9096cac13c
Alex Cattani <a.cattani44@gmail.com>
2024-04-18 13:50:18 -0400

    Added last component scaffolding, specified props, added extra pictures

---
+++ volunteer-web/src/components/Card.jsx
@@ -0,0 +1,8 @@
+{/* This card will be used for charities, events, and jobs.
+    If necessary, an enum can specify the design for each card.
+    For now they will all have the same design. 
+    Props: image, title, overview, buttonLink, buttonText   */}
+
+    export default function Card(props) {
+
+    };
---
+++ volunteer-web/src/components/CharityPage.jsx
@@ -0,0 +1,8 @@
+{/* This will be a component that takes the charity pages hardcoded
+    into opportunities page and linked accordingly
+    Props: bannerImg, Position[] with Title, Overview, Hours, Location,
+     Contact, About, Images[] */}
+
+    export default function CharityPage(props) {
+
+    };
